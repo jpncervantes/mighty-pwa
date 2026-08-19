8eadc535bb1c74d2077b72f8187bd54300cd7fe1
jpncervantes <johnpaulncervantes@gmail.com>
2026-08-19 14:51:54 +0800

    added assets in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,31 @@ function App() {
   return (
     <div>
       <Navbar />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '50px',
+        width: '100%',
+        gap: '20px'
+      }}>
+        <img src="/sinulog2026dekstop1.png" width={'50%'} style={{
+          minWidth: '300px'
+        }}/>
+        <img src="/sinulog2026dekstop1.png" width={'50%'} style={{
+          minWidth: '300px'
+        }}/>
+        <img src="/sinulog2026dekstop1.png" width={'50%'} style={{
+          minWidth: '300px'
+        }}/>
+        <img src="/sinulog2026dekstop1.png" width={'50%'} style={{
+          minWidth: '300px'
+        }}/>
+        <img src="/sinulog2026dekstop1.png" width={'50%'} style={{
+          minWidth: '300px'
+        }}/>
+      </div>
     </div>
   )
 }
